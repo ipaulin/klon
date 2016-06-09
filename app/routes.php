ec38a6eea0ad57f4cad65180df658ac16a104072
Ivan Paulin <paulin.ivan.1@gmail.com>
2016-06-09 22:52:12 +0200

    apply regex on some routes

--- app/routes.php
+++ app/routes.php
@@ -18,7 +18,8 @@ $this->router->get('logout', '\Controllers\AuthController@logoutAction');
  */
 $this->router->get('user/{id}/{big_data?}', '\Controllers\UserController@getUserAction')->where('id', '[0-9]+');
 $this->router->post('user/{id}/edit', '\Controllers\UserController@editAction')->where('id', '[0-9]+');
-$this->router->get('user/search/{query?}/{limit?}/{offset?}', '\Controllers\UserController@searchAction');
+$this->router->get('user/search/{query?}/{limit?}/{offset?}', '\Controllers\UserController@searchAction')
+    ->where(['limit' => '[0-9]+', 'offset' => '[0-9]+']);
 $this->router->post('user/{id}/follow', '\Controllers\UserController@followAction');
 $this->router->post('user/{id}/unfollow', '\Controllers\UserController@unfollowAction');
 
@@ -27,5 +28,7 @@ $this->router->post('user/{id}/unfollow', '\Controllers\UserController@unfollowA
  */
 $this->router->post('statuses/create', '\Controllers\StatusesController@createAction');
 $this->router->get('statuses/{id}', '\Controllers\StatusesController@getStatusAction')->where('id', '[0-9]+');
-$this->router->get('statuses/user-timeline/{user_id}/{limit}/{offset}/{page?}', '\Controllers\StatusesController@getUserStatusesAction');
-$this->router->get('statuses/home', '\Controllers\StatusesController@getStatusesHomeAction');
+$this->router->get('statuses/user-timeline/{user_id}/{limit}/{offset}/{page?}', '\Controllers\StatusesController@getUserStatusesAction')
+    ->where(['user_id' => '[0-9]+', 'limit' => '[0-9]+', 'offset' => '[0-9]+', 'page' => '[0-9]+']);
+$this->router->get('statuses/home/{limit}/{offset}/{page?}', '\Controllers\StatusesController@getStatusesHomeAction')
+    ->where(['limit' => '[0-9]+', 'offset' => '[0-9]+', 'page' => '[0-9]+']);
